67020354e7f11eeff7ec8527c6c0cacb2b301a1e
Lê Văn Hậu <levanhau@MacBook-Pro-cua-Hau.local>
2026-02-10 16:21:40 +0700

    feat: bổ sung trường Address vào Model Student và cập nhật giao diện hiển thị

--- Models/Student.cs
+++ Models/Student.cs
@@ -5,4 +5,5 @@ public class Student
     public int Id { get; set; }
     public string FullName { get; set; } = string.Empty;
     public double Gpa { get; set; }
+    public String Address { get; set;} = string.Empty;
 }
